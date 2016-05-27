58a3d706962d6974a9aff581864436d5da7e27f4
Inverse Integral <inverseintegral42@gmail.com>
2016-05-28 00:17:18 +0200

    Improved the main javascript file. Moved the models to a new subfolder.

--- app.js
+++ app.js
@@ -1,54 +1,44 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-var todoRoutes = require('./routes/todo');
+const port = process.env.PORT || 3000;
+const app = express();
 
-var app = express();
+/* Custom routers */
+const todoRoutes = require('./routes/todo');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', todoRoutes);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+/**
+ * Starts the express server and listens on the given port.
+ */
+const listen = () => {
+    app.listen(port);
+    console.log('Express app started on port ' + port);
+};
 
-// error handlers
+/**
+ * Connects to the mongod with the given configuration parameters.
+ * @returns {MongooseThenable} a monngoose abstraction
+ */
+const connect = () => {
+    var options = {server: {socketOptions: {keepAlive: 1}}};
+    return mongoose.connect('mongodb://localhost/neverresting', options).connection;
+};
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
+connect()
+    .on('error', console.log)
+    .on('disconnected', connect)
+    .once('open', listen);
 
 module.exports = app;
