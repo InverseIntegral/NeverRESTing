0348f400dae0a75004bdc8989f0e1bd7829aadb7
Inverse Integral <inverseintegral42@gmail.com>
2016-05-28 23:17:51 +0200

    Added create date to the entity. Added request validation (WIP).

--- routes/todo.js
+++ routes/todo.js
@@ -1,11 +1,34 @@
 var router = require('express').Router();
 var ToDo = require('../app/models/ToDo');
-var ResponseHandler = require('../app/responseHandler');
+
+const handlePromise = (promise, response) => {
+    let ResponseHandler = require('../app/responseHandler');
+    const handler = new ResponseHandler(response);
+
+    promise
+        .then(data => handler.handleSuccess(data))
+        .catch(error => handler.handleFailure(error));
+};
+
+const validate = (...check) => {
+    for (let value of check) {
+        if (value == null) {
+            return false;
+        }
+    }
+
+    return true;
+};
 
 /* POST request that creates a new todo */
 router.post('/todo', (req, res) => {
     // Destructuring the request body
     const {name} = req.body;
+
+    if (!validate(name)) {
+        res.status(400).json('Invalid request');
+        return;
+    }
 
     // Create the new instance with the data
     const instance = new ToDo();
@@ -13,21 +36,19 @@ router.post('/todo', (req, res) => {
 
     // Save the new todo
     const promise = instance.save();
-    const handler = new ResponseHandler(res);
-
-    promise.then(data => handler.handleSuccess(data))
-        .catch(error => handler.handleFailure(error));
+    handlePromise(promise, res);
 });
 
 /* GETs all the todos */
 router.get('/todos', (req, res) => {
-
     const promise = ToDo.find({}).exec();
-    const handler = new ResponseHandler(res);
+    handlePromise(promise, res);
+});
 
-    promise.then(data => handler.handleSuccess(data))
-        .catch(error => handler.handleFailure(error));
-
+/* GETs a specific todo by its id */
+router.get('/todo/:id', (req, res) => {
+    const promise = ToDo.findOne({_id: req.params.id}).exec();
+    handlePromise(promise, res);
 });
 
 module.exports = router;
