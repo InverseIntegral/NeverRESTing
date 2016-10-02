8e724110f4a3986d42521e5582d06db2e7c6ccf2
Inverse Integral <InverseIntegral@users.noreply.github.com>
2016-10-02 10:27:21 +0200

    Added redux async support. Renamed the name property of the todo to text.

--- public/javascripts/actions/index.js
+++ public/javascripts/actions/index.js
@@ -1,10 +1,11 @@
-let nextTodoId = 0;
+import rest from 'rest';
+import mime from 'rest/interceptor/mime';
+var client = rest.wrap(mime);
 
-export const addTodo = (text) => {
+export const receiveTodo = (json) => {
     return {
-        type: 'ADD_TODO',
-        id: nextTodoId++,
-        text
+        type: 'RECEIVE_TODO',
+        state: json
     }
 };
 
@@ -14,3 +15,58 @@ export const removeTodo = (id) => {
         id: id
     };
 };
+
+export const requestTodos = () => {
+    return {
+        type: 'REQUEST_TODOS'
+    };
+};
+
+export const receiveTodos = (json) => {
+    return {
+        type: 'RECEIVE_TODOS',
+        state: json
+    };
+};
+
+export function fetchTodos() {
+    return (dispatch) => {
+        dispatch(requestTodos());
+
+        return client('http://localhost:3000/todos')
+            .then(response => {
+                dispatch(receiveTodos(response.entity))
+            });
+    };
+}
+
+export function addTodo(text) {
+    return (dispatch) => {
+        dispatch(requestTodos());
+
+        return client({
+            'path': 'http://localhost:3000/todos',
+            'headers': {
+                'Content-Type': 'application/json'
+            },
+            'entity': {
+                text
+            }
+        }).then(response => {
+            dispatch(receiveTodo(response.entity))
+        });
+    }
+}
+
+export function deleteTodo(id) {
+    return (dispatch) => {
+        dispatch(requestTodos());
+
+        return client({
+            'path': 'http://localhost:3000/todos/' + id,
+            'method': 'DELETE'
+        }).then(() => {
+            dispatch(removeTodo(id))
+        });
+    }
+}
