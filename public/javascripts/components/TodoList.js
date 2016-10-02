0c23f7b014edd3b522563f4aab8e5e5949eb4252
Inverse Integral <inverseintegral42@gmail.com>
2016-10-02 10:27:21 +0200

    Added redux async support. Renamed the name property of the todo to text.

--- public/javascripts/components/TodoList.js
+++ public/javascripts/components/TodoList.js
@@ -1,13 +1,12 @@
-import React, { PropTypes } from 'react'
+import React, {PropTypes} from 'react'
 import Todo from './Todo'
 
-const TodoList = ({ todos, onTodoClick }) => (
+const TodoList = ({todos, onTodoClick}) => (
     <ul>
         {todos.map(todo =>
-            <Todo
-                key={todo.id}
+            <Todo key={todo._id}
                 {...todo}
-                onClick={() => onTodoClick(todo.id)}
+                onClick={() => onTodoClick(todo._id)}
             />
         )}
     </ul>
@@ -15,7 +14,7 @@ const TodoList = ({ todos, onTodoClick }) => (
 
 TodoList.propTypes = {
     todos: PropTypes.arrayOf(PropTypes.shape({
-        id: PropTypes.number.isRequired,
+        _id: PropTypes.string.isRequired,
         text: PropTypes.string.isRequired
     }).isRequired).isRequired,
     onTodoClick: PropTypes.func.isRequired
