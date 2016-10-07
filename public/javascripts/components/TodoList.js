6ef286bd3c7dceda83440f5c89381007928da8df
Inverse Integral <inverseintegral42@gmail.com>
2016-10-07 12:53:32 +0200

    Made list item as a conditional component

--- public/javascripts/components/TodoList.js
+++ public/javascripts/components/TodoList.js
@@ -1,25 +1,30 @@
 import React, {PropTypes} from 'react'
 import Todo from './Todo'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import { If, Then, Else } from 'react-if';
 
 const TodoList = ({todos, onTodoClick}) => {
     return (
         <div className="item">
-            <ul className="collection">
-                <ReactCSSTransitionGroup
-                    transitionName="fade"
-                    transitionEnterTimeout={500}
-                    transitionLeaveTimeout={300}>
-                    {todos.map(todo =>
-                        <Todo key={todo._id}
-                              {...todo}
-                              onClick={() => {
-                                  onTodoClick(todo._id);
-                              }}
-                        />
-                    )}
-                </ReactCSSTransitionGroup>
-            </ul>
+            <If condition={todos.length !== 0}>
+              <Then>
+                <ul className="collection" >
+                    <ReactCSSTransitionGroup
+                        transitionName="fade"
+                        transitionEnterTimeout={500}
+                        transitionLeaveTimeout={300}>
+                        {todos.map(todo =>
+                            <Todo key={todo._id}
+                                  {...todo}
+                                  onClick={() => {
+                                      onTodoClick(todo._id);
+                                  }}
+                            />
+                        )}
+                    </ReactCSSTransitionGroup>
+                </ul>
+              </Then>
+            </If>
         </div>
     );
 };
