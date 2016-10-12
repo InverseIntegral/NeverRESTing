aded33cd8bbba2ef48ec4baed37679e9404d3692
Inverse Integral <InverseIntegral@users.noreply.github.com>
2016-10-12 09:02:23 +0200

    Worked on #13.

--- public/javascripts/components/App.js
+++ public/javascripts/components/App.js
@@ -8,7 +8,6 @@ const App = () => (
         <div>
             <h1>NeverRESTing</h1>
         </div>
-        <a href="https://github.com/login/oauth/authorize?scope=user:email&client_id=f33f36e205375acb0335">Click here</a>
         <AddTodo/>
         <ConnectedSpinner/>
         <ConnectedTodoList/>
